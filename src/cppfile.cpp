182f7b9d50a33a523af397ff2eb2bff743273469
stla <laurent_step@yahoo.fr>
2018-09-16 21:32:13 +0200

    first commit

---
+++ src/cppfile.cpp
@@ -0,0 +1,110 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+// we only include RcppEigen.h which pulls Rcpp.h in for us
+#include <RcppEigen.h>
+#include <random>
+#include <vector>
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppEigen so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppEigen)]]
+
+typedef Eigen::Matrix<long double, Eigen::Dynamic, Eigen::Dynamic> MatrixXl;
+typedef Eigen::Matrix<long double, Eigen::Dynamic, 1> VectorXl;
+
+// [[Rcpp::export]]
+Eigen::VectorXd llinsolve(const Eigen::MatrixXd & A, const Eigen::VectorXd & b){
+  MatrixXl aa = A.cast<long double>();
+  VectorXl bb = b.cast<long double>();
+  VectorXl xx = aa.colPivHouseholderQr().solve(bb);
+  Eigen::VectorXd x = xx.cast<double>();
+  return x;
+} 
+
+// [[Rcpp::export]]
+Eigen::MatrixXd tsolveAndMultiply(const Eigen::MatrixXd & A, const Eigen::MatrixXd & C){
+  Eigen::MatrixXd M = A.triangularView<Eigen::Upper>().solve<Eigen::OnTheRight>(C);
+  return M; // C %*% solve(A)
+} 
+
+// [[Rcpp::export]]
+Rcpp::List nullSpace(const Eigen::MatrixXd M){
+  Eigen::FullPivLU<Eigen::MatrixXd> lu(M);
+  Eigen::MatrixXd nspace = lu.kernel(); // not orthonormal
+  int r = lu.rank();
+  return Rcpp::List::create(Rcpp::Named("kernel") = nspace,
+                            Rcpp::Named("rank") = r);
+}
+
+// [[Rcpp::export]]
+Rcpp::List QRdecomp(const Eigen::MatrixXd & M){ // for nrows >= ncols
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr = M.householderQr();
+  Eigen::MatrixXd R_ = qr.matrixQR().triangularView<Eigen::Upper>();
+  Eigen::MatrixXd Q_ = qr.householderQ();
+  Eigen::MatrixXd R = R_.block(0,0,M.cols(),M.cols());
+  Eigen::MatrixXd Q = Q_.block(0,0,M.rows(),M.cols());
+  return Rcpp::List::create(Rcpp::Named("Q") = Q,
+                            Rcpp::Named("R") = R);
+}
+  
+// [[Rcpp::export]]
+Eigen::VectorXd whichLU(const Eigen::VectorXd & V, double L, double U){
+  std::vector<size_t> whichl, checkl, whichu, checku;
+  for(size_t i=0; i<V.size(); i++){
+    if(V(i) <= L){
+      whichl.push_back(i);
+    }else{
+      checkl.push_back(i);
+    }
+  }
+  Eigen::VectorXd W(whichl.size());
+  for(size_t i=0; i < whichl.size(); i++){
+    W(i) = V(whichl[i]);
+  }
+  return W;
+}
+
+// [[Rcpp::export]]
+int main(){
+  Eigen::VectorXd V(8);
+  V << 0.0, 1.0, 5.0, 4.0, 9.0, 1.0, 2.0, 3.0;
+  Eigen::VectorXd W = whichLU(V, 3.0, 10000.0);
+  std::cout << W << std::endl;
+  return 0;
+}
+
+std::default_random_engine generator;
+std::normal_distribution<double> distribution(0.0,1.0);
+
+Eigen::MatrixXd gmatrix(size_t nrows, size_t ncols){
+  Eigen::MatrixXd G(nrows,ncols);
+  for(size_t i=0; i<nrows; i++){
+    for(size_t j=0; j<ncols; j++){
+      G(i,j) = distribution(generator);
+    }
+  }
+  return G;
+}
+
+
+// [[Rcpp::export]]
+std::vector<Eigen::MatrixXd> ListOfGmatrices(
+    size_t nrows, size_t ncols, size_t nmatrices){
+  std::vector<Eigen::MatrixXd> matrices(2);
+  for(unsigned i=0; i<nmatrices; i++){
+    matrices[i] = gmatrix(nrows,ncols);
+  }
+  for(unsigned i=0; i<nmatrices; i++){
+    std::cout << matrices[i] << std::endl;
+  }
+  return matrices;
+}
+
+// [[Rcpp::export]]
+int temp(unsigned n){
+  int x = 0;
+  for(unsigned i=0; i<n; i++){
+    x = x+1;
+  }
+  return x;
+}
